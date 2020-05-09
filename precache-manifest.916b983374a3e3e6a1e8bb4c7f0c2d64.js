8c3e5df08b2a4e887091e66c8ac5732cb16e4b16
tejas1597 <tjs1597@gmail.com>
2020-05-09 12:54:31 +0530

    Updates

---
+++ precache-manifest.916b983374a3e3e6a1e8bb4c7f0c2d64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e89f011fa0a8b2078ce20c624d12ca",
+    "url": "/mons-rolo/index.html"
+  },
+  {
+    "revision": "98f1e81719e381562039",
+    "url": "/mons-rolo/static/css/main.5512e96d.chunk.css"
+  },
+  {
+    "revision": "9138adc3f668dc6cf19e",
+    "url": "/mons-rolo/static/js/2.3878d60c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mons-rolo/static/js/2.3878d60c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f1e81719e381562039",
+    "url": "/mons-rolo/static/js/main.01678dc0.chunk.js"
+  },
+  {
+    "revision": "387b2af155927347fb97",
+    "url": "/mons-rolo/static/js/runtime-main.4ca9360e.js"
+  }
+]);
